80717e88e304249926815861f203dc2e782d39ed
Jan Brinkmann <lucky@the-luckyduck.de>
2012-09-02 15:15:55 +0200

    plugin uri

--- simple-multisite-sitemaps.php
+++ simple-multisite-sitemaps.php
@@ -1,6 +1,7 @@
 <?php
 /*
 Plugin Name: Simple Multisite Sitemaps
+Plugin URI: https://github.com/luckyduck/Simple-Multisite-Sitemaps
 Description: A very simple solution for generating seperate Google XML Sitemaps for each WordPress site in a multisite network, on-the-fly.
 Version: 1.0
 Author: Jan Brinkmann
@@ -45,7 +46,7 @@ add_action( 'init', 'jb_sms_sitemap_flush_rules' );
 
 // rewrite
 function jb_sms_xml_feed_rewrite($wp_rewrite) {
-	global $wp_rewrite;
+    global $wp_rewrite;
 
     $feed_rules = array(
         '.*sitemap.xml$' => 'index.php?feed=sitemap'
@@ -57,14 +58,14 @@ add_filter( 'generate_rewrite_rules', 'jb_sms_xml_feed_rewrite' );
 
 // generate sitemap.xml using the template
 function jb_sms_generate_sitemap() {
-	$template_dir = dirname(__FILE__);
-	load_template( $template_dir . '/sitemap-template.php' );
+    $template_dir = dirname(__FILE__);
+    load_template( $template_dir . '/sitemap-template.php' );
 }
 add_action( 'do_feed_sitemap', 'jb_sms_generate_sitemap', 10, 1 );
 
 // add to robots.txt
 function jb_sms_add_sitemap_to_robotstxt() {
-	echo "Sitemap: ".get_option('siteurl')."/sitemap.xml\n\n";
+    echo "Sitemap: ".get_option('siteurl')."/sitemap.xml\n\n";
 }
 add_action( 'do_robotstxt', 'jb_sms_add_sitemap_to_robotstxt' );
 
